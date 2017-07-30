9bafe33331d8a0f51dc21befe2a7aa5d993e7149
Noel Delgado <auhcsei@gmail.com>
2017-07-29 20:50:57 -0500

    update / gh-pages, use latest tarima

--- docs/rollup.config.js
+++ docs/rollup.config.js
@@ -1,16 +1,19 @@
-var buble = require('rollup-plugin-buble');
-var nodeResolve = require('rollup-plugin-node-resolve');
-var commonjs = require('rollup-plugin-commonjs');
+const json = require('rollup-plugin-json');
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const buble = require('rollup-plugin-buble');
 
+// `entry` and `dest` properties are omitted in purpose
+// this configuration file is consume by tarima
+// https://github.com/tacoss/tarima#38---rollupjs
 module.exports = {
+  // entry: './src/js/_entries/main.js',
+  // dest: './test/test.js',
   format: 'cjs',
   plugins: [
-    nodeResolve({
-      jsnext: true,
-      main: true,
-      browser: true
-    }),
+    resolve(),
     commonjs(),
+    json(),
     buble()
   ]
 };
