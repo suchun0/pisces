5da177f5c593ecfdcae4cbc091c6ac906348b3b6
Noel Delgado <auhcsei@gmail.com>
2017-07-29 20:44:46 -0500

    update / use get-scrollingelement ES2015 module

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
 import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
 import buble from 'rollup-plugin-buble';
 
 export default {
   entry: './src/index.js',
+  dest: './lib/index.js',
   format: 'umd',
   moduleName: 'Pisces',
   plugins: [
     json(),
+    resolve(),
     buble()
-  ],
-  dest: './lib/index.js'
+  ]
 };
