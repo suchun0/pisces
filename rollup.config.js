67d676e6d270661e329a3becb618c091f00fd55b
Noel Delgado <auhcsei@gmail.com>
2017-08-02 21:00:35 -0500

    create new `lib/index.es.js` file using `format: es`

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,27 @@ import json from 'rollup-plugin-json';
 import resolve from 'rollup-plugin-node-resolve';
 import buble from 'rollup-plugin-buble';
 
-export default {
-  entry: './src/index.js',
-  dest: './lib/index.js',
-  format: 'umd',
-  moduleName: 'Pisces',
-  plugins: [
-    json(),
-    resolve(),
-    buble()
-  ]
-};
+export default [
+  {
+    entry: './src/index.js',
+    dest: './lib/index.js',
+    format: 'umd',
+    moduleName: 'Pisces',
+    plugins: [
+      json(),
+      resolve(),
+      buble()
+    ]
+  },
+  {
+    entry: './src/index.js',
+    dest: './lib/index.es.js',
+    format: 'es',
+    external: [
+      'get-scrollingelement'
+    ],
+    plugins: [
+      json()
+    ]
+  }
+];
